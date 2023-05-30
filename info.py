f7c14ec1a9a502e41c49c7e2a53c6fe3c3635a76
kkiinngg620 <133698348+kkiinngg620@users.noreply.github.com>
2023-05-30 14:58:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,7 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'AutofilterBot')
 API_ID = int(environ.get('API_ID', '27157998'))
 API_HASH = environ.get('API_HASH', '45d09c93e37c9b93b6535949c898f906')
-BOT_TOKEN = environ.get('BOT_TOKEN', "5772790410:AAHc77rHJexkbVZiaLrv0CJed3qo9OzBnjE")
+BOT_TOKEN = environ.get('BOT_TOKEN', "6057056834:AAHN3QTTNtMI9Lcibn-_w-E4Uevl5DVOHHs")
 
 #Port
 PORT = environ.get("PORT", "8080")
@@ -22,32 +22,32 @@ PORT = environ.get("PORT", "8080")
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/fbf4efda024d766f755a4.jpg')).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/6cc72163d754fd5f1db0b.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1452198353').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001532961331').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1452198353 5403432874').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001631824320').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL', '-1001856835711')
+auth_channel = environ.get('AUTH_CHANNEL', '-1001814749004')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Closter0:closter123@cluster0.ia3u0ir.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Closter0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mdsalman098123:mdsalman098123@cluster0.jcnrac7.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "mdsalman098123")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_Files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001637368751'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001883653903'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'moviieeadda3')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), False)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), False)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "<b>Title:</b> <code>{file_caption}</code>\n\n⚡<b>File uploaded by [Movies Adda™](https://t.me/moviieeadda3)</b> \n\n⚙️ <b>Size: </b><i>{file_size}</i>\n\n👉 <b>Join Now : @moviieeadda3</b> 👈")
-BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", "<b>Title:</b> <code>{file_caption}</code> @moviieeadda3 \n\n⚙️ <b>Size: </b><i>{file_size}</i>\n\n👉 <b>Join Now : @moviieeadda3</b> 👈")
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Your Query: {query}</b> \n<b>‌‌‌‌IMDb Data by: @moviieeadda3 </b> \n\n<b>🍿 Title: {title}</b>\n<b>🌟 Rating : {rating}/10</b>\n<b>🎭 Genres: {genres}</b>\n<b>📆 Year: {year}</b>\n<b>⏰ Duration : {runtime} minutes</b>\n\n<b>♥️ Please Share Us ♥️</b>")
+CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "<b>Title:</b> <code>{file_caption}</code>\n\n⚡<b>File uploaded by [Movies Adda™](https://t.me/Moviesbank_Corporation)</b> \n\n⚙️ <b>Size: </b><i>{file_size}</i>\n\n👉 <b>Join Now : @Moviesbank_Corporation</b> 👈")
+BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", "<b>Title:</b> <code>{file_caption}</code> @Moviesbank_Corporation \n\n⚙️ <b>Size: </b><i>{file_size}</i>\n\n👉 <b>Join Now : @Moviesbank_Corporation</b> 👈")
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Your Query: {query}</b> \n<b>‌‌‌‌IMDb Data by: @Moviesbank_Corporation </b> \n\n<b>🍿 Title: {title}</b>\n<b>🌟 Rating : {rating}/10</b>\n<b>🎭 Genres: {genres}</b>\n<b>📆 Year: {year}</b>\n<b>⏰ Duration : {runtime} minutes</b>\n\n<b>♥️ Please Share Us ♥️</b>")
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), False)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
@@ -71,7 +71,7 @@ LAZY_MODE = bool(environ.get("LAZY_MODE", "true"))
 lazy_renamers = [int(lazrenamers) if id_pattern.search(lazrenamers) else lazrenamers for lazrenamers in environ.get('LAZY_RENAMERS', '1452198353').split()]
 LAZY_RENAMERS = (lazy_renamers + ADMINS) if lazy_renamers else []
 # Only Give Value in LAZY_RENAMERS if you have enabled LAZY_MODE ----- !
-REQ_CHANNEL = int(environ.get('REQ_CHANNEL', '-1001988418193'))
+REQ_CHANNEL = int(environ.get('REQ_CHANNEL', ''))
 #   REQ_CHANNEL is for the logs of that content name which is not found in group -- !
 URL_MODE = is_enabled((environ.get("URL_MODE", "False")), True)
 # Use True false in url mode => Set value true if you want shortlinks - else - use value False ----- !
@@ -88,11 +88,11 @@ if SELF_DELETE == "True":
 
 # Download Tutorial Button #
 DOWNLOAD_TEXT_NAME = "📥 Join Backup Channel 📥"
-DOWNLOAD_TEXT_URL = "https://moviieeadda2"
+DOWNLOAD_TEXT_URL = "https://Moviesbank_Corporation"
 
 # Custom Caption Under Button #
 CAPTION_BUTTON = "Get Updates"
-CAPTION_BUTTON_URL = "https://t.me/moviieeadda3"
+CAPTION_BUTTON_URL = "https://t.me/MOVIES_BANK_2"
 
 # _______________________________________________________________________________________________________________ #
 # __________________________________________Credit_______________________________________________________________ #
