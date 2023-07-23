2b4ab9b36b9ddc9c377e24cded74ae7a3125710e
kkiinngg620 <133698348+kkiinngg620@users.noreply.github.com>
2023-07-24 00:29:41 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -889,7 +889,7 @@ async def auto_filter(client, msg, spoll=False):
                 btn = [
                     [
                         InlineKeyboardButton(
-                            text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                            text=f"[📁{get_size(file.file_size)}▷] {file.file_name}", callback_data=f'{pre}#{file.file_id}'
                         ),
                     ]
                     for file in files
@@ -898,7 +898,7 @@ async def auto_filter(client, msg, spoll=False):
                 btn = [
                     [
                         InlineKeyboardButton(
-                            text=f"[{get_size(file.file_size)}] {file.file_name}", 
+                            text=f"[📁{get_size(file.file_size)}▷] {file.file_name}", 
                             url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}")
                         ),
                     ]
@@ -923,11 +923,11 @@ async def auto_filter(client, msg, spoll=False):
             btn = [
                 [
                     InlineKeyboardButton(
-                        text=f"[{get_size(file.file_size)}] {file.file_name}", 
+                        text=f"[📁{get_size(file.file_size)}▷] {file.file_name}", 
                         url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}")
                     ),
                     InlineKeyboardButton(
-                        text=f"[{get_size(file.file_size)}] {file.file_name}", 
+                        text=f"[📁{get_size(file.file_size)}▷] {file.file_name}", 
                         url=await get_shortlink(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}")
                     ),
                 ]
